dd6db4ae8846527a8eb32808c16d0558eb2f4fb1
Pierre Bourgès <pierre.bourges@ynov.com>
2019-10-11 15:01:41 +0200

    Design ArtistWidget Color, placement and alignement

--- lib/widget/ArtistWidget.dart
+++ lib/widget/ArtistWidget.dart
@@ -9,33 +9,45 @@ class ArtistWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black54,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  child: Image.network(
-                    imageUrl,
-                    height: 200,
-                    width: 200,
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Material(
+        elevation: 8,
+        borderRadius: BorderRadius.only(topRight: Radius.circular(40), bottomRight: Radius.circular(40)),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Color(0xffBE5A38),
+              borderRadius: BorderRadius.only(topRight: Radius.circular(40), bottomRight: Radius.circular(40),),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  child: Container(
+                    height: 75,
+                    width: 75,
+                    child: Image.network(
+                      imageUrl,
+                      height: 200,
+                      width: 200,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Text(
-              artistName,
-              style: TextStyle(color: Colors.white),
-            )
-          ],
+              Text(
+                artistName,
+                style: TextStyle(
+                  color: Color(0xFFF7EDDC),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
