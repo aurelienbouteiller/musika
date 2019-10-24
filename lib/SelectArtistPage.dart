4e170f42efadbfd64da0805244e5ba48deb76211
Vincent Roquelaure <roquelaurevincent@gmail.com>
2019-10-24 09:14:20 +0200

    Level screen and position

--- lib/SelectArtistPage.dart
+++ lib/SelectArtistPage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:musika/model/Artist.dart';
 
 class SelectArtistPage extends StatefulWidget {
-  SelectArtistPage({Key key}) : super(key: key);
+  SelectArtistPage({Key key, this.niveau}) : super(key: key);
+
+  final String niveau;
 
   @override
   _SelectArtistPageState createState() => _SelectArtistPageState();
@@ -16,10 +17,6 @@ class _SelectArtistPageState extends State<SelectArtistPage> {
   @override
   void initState() {
     super.initState();
-
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
 
     artistes = [
       Artist(id: 1, name: "Parme San"),
@@ -34,63 +31,92 @@ class _SelectArtistPageState extends State<SelectArtistPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(
-              child: Text("Sélection de l'artiste",
-                  style: TextStyle(color: Theme.of(context).accentColor))),
+          title: Text("Sélection de l'artiste",
+              style: TextStyle(color: Theme.of(context).accentColor)),
           backgroundColor: Theme.of(context).backgroundColor,
         ),
         backgroundColor: Theme.of(context).backgroundColor,
-        body: ListView.builder(
-            itemCount: artistes.length,
-            itemBuilder: (context, index) {
-              return Card(
-                elevation: 4.5,
-                color: Colors.transparent,
-                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5.0),
-                  child: Container(
-                    decoration:
-                        BoxDecoration(color: Theme.of(context).primaryColor),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 10.0),
-                      leading: Container(
-                        padding: EdgeInsets.only(right: 12.0),
-                        decoration: BoxDecoration(
-                            border: Border(
-                                right: BorderSide(
-                                    width: 1.0,
-                                    color: Theme.of(context).dividerColor))),
-                        child: CircleAvatar(
-                          radius: 25.0,
-                          backgroundImage: NetworkImage(
-                            "https://api.deezer.com/artist/27/image",
-                          ),
-                        ),
-                      ),
-                      title: Text(artistes[index].name,
-                          style: TextStyle(
-                              color: Theme.of(context).secondaryHeaderColor,
-                              fontWeight: FontWeight.bold)),
-                      trailing: Icon(Icons.keyboard_arrow_right,
-                          color: Theme.of(context).secondaryHeaderColor),
-                      onTap: () {
-                        print(artistes[index].name);
-                      },
-                      subtitle: Row(
-                        children: <Widget>[
-                          Icon(Icons.linear_scale,
-                              color: Theme.of(context).accentColor),
-                          Text(" 0/5",
-                              style: TextStyle(
-                                  color: Theme.of(context).accentColor))
-                        ],
-                      ),
+        body: Column(
+          children: <Widget>[
+            Hero(
+              tag: widget.niveau,
+              child: SizedBox(
+                width: double.infinity,
+                height: 125.0,
+                child: Card(
+                  elevation: 4.5,
+                  color: Colors.transparent,
+                  margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: Container(
+                      decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColor),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                        child: Text(widget.niveau),
+                      )
                     ),
                   ),
                 ),
-              );
-            }));
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: artistes.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 4.5,
+                      color: Colors.transparent,
+                      margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5.0),
+                        child: Container(
+                          decoration:
+                              BoxDecoration(color: Theme.of(context).primaryColor),
+                          child: ListTile(
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            leading: Container(
+                              padding: EdgeInsets.only(right: 12.0),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      right: BorderSide(
+                                          width: 1.0,
+                                          color: Theme.of(context).dividerColor))),
+                              child: CircleAvatar(
+                                radius: 25.0,
+                                backgroundImage: NetworkImage(
+                                  "https://api.deezer.com/artist/27/image",
+                                ),
+                              ),
+                            ),
+                            title: Text(artistes[index].name,
+                                style: TextStyle(
+                                    color: Theme.of(context).secondaryHeaderColor,
+                                    fontWeight: FontWeight.bold)),
+                            trailing: Icon(Icons.keyboard_arrow_right,
+                                color: Theme.of(context).secondaryHeaderColor),
+                            onTap: () {
+                              print(artistes[index].name);
+                            },
+                            subtitle: Row(
+                              children: <Widget>[
+                                Icon(Icons.linear_scale,
+                                    color: Theme.of(context).accentColor),
+                                Text(" 0/5",
+                                    style: TextStyle(
+                                        color: Theme.of(context).accentColor))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flare_flutter/flare_controls.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:musika/ApiDeezer.dart';
+import 'package:musika/SelectArtistPage.dart';
+import 'package:musika/SelectLevelPage.dart';
 import 'package:musika/widget/ArtistWidget.dart';
 import 'package:musika/widget/ChoiceWidget.dart';
 import 'package:musika/widget/MusicManager.dart';
@@ -19,8 +21,12 @@ void main() => runApp(MusikaApp());
 class MusikaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return new MaterialApp(
-      home: new GuessSongPage(),
+      home: new SelectLevelPage(),
       theme: new ThemeData(
           brightness: Brightness.light,
           primaryColor: Color(0xFFfdcb6e),
@@ -64,9 +70,6 @@ class _GuessSongPageState extends State<GuessSongPage> {
   @override
   void initState() {
     super.initState();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
 
     selectedTracks = List<Track>();
     audioPlayer = AudioPlayer();
