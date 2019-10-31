aecaa7a23f3ab83f4c3676a9f00983b175ba9c61
Aurélien Bouteiller <aurelien.bouteiller@ynov.com>
2019-10-31 18:00:28 +0100

    Extract MenuBar and Sign up widgets

---
+++ lib/authentification/widgets/MenuBar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:musika/widget/bubble_indication_painter.dart';
+
+class MenuBar extends StatelessWidget {
+  final Color rightColor;
+  final Color leftColor;
+  PageController pageController;
+
+  MenuBar({this.rightColor, this.leftColor, this.pageController});
+
+  void _onSignInButtonPress() {
+    pageController.animateToPage(0,
+        duration: Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  void _onSignUpButtonPress() {
+    pageController?.animateToPage(1,
+        duration: Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: Color(0x552B2B2B),
+        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+      ),
+      child: CustomPaint(
+        painter: TabIndicationPainter(pageController: pageController),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: _onSignInButtonPress,
+                child: Text(
+                  "Existing",
+                  style: TextStyle(
+                      color: leftColor,
+                      fontSize: 16.0,
+                      fontFamily: "WorkSansSemiBold"),
+                ),
+              ),
+            ),
+            //Container(height: 33.0, width: 1.0, color: Colors.white),
+            Expanded(
+              child: FlatButton(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: _onSignUpButtonPress,
+                child: Text(
+                  "New",
+                  style: TextStyle(
+                      color: rightColor,
+                      fontSize: 16.0,
+                      fontFamily: "WorkSansSemiBold"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/authentification/widgets/SignUp.dart
@@ -0,0 +1,296 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:musika/style/theme.dart' as Theme;
+
+import '../../SelectLevelPage.dart';
+
+class SignUp extends StatefulWidget {
+  final auth;
+
+  SignUp({this.auth});
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final GlobalKey<FormState> formKeySignUp = GlobalKey<FormState>();
+  final GlobalKey<FormState> _containerKeySignUp = GlobalKey<FormState>();
+  String _emailSignUp, _passwordSignUp;
+  final FocusNode myFocusNodeName = FocusNode();
+  final FocusNode myFocusNodeEmail = FocusNode();
+  final FocusNode myFocusNodePassword = FocusNode();
+  bool _obscureTextSignup = true;
+  bool _obscureTextSignupConfirm = true;
+
+  TextEditingController signupNameController = new TextEditingController();
+  TextEditingController signupEmailController = new TextEditingController();
+  TextEditingController signupPasswordController = new TextEditingController();
+  TextEditingController signupConfirmPasswordController =
+      new TextEditingController();
+
+  void _signUp(context) async {
+    final _formState = formKeySignUp.currentState;
+    if (_formState.validate()) {
+      _formState.save();
+      try {
+        FirebaseUser user = (await widget.auth.createUserWithEmailAndPassword(
+                email: _emailSignUp, password: _passwordSignUp))
+            .user;
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => SelectLevelPage()));
+      } catch (e) {
+        print(e.message);
+      }
+    }
+  }
+
+  void _toggleSignup() {
+    setState(() {
+      _obscureTextSignup = !_obscureTextSignup;
+    });
+  }
+
+  void _toggleSignupConfirm() {
+    setState(() {
+      _obscureTextSignupConfirm = !_obscureTextSignupConfirm;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 23.0),
+      child: Column(
+        children: <Widget>[
+          Form(
+            key: formKeySignUp,
+            child: Container(
+              key: _containerKeySignUp,
+              child: Stack(
+                alignment: Alignment.topCenter,
+                overflow: Overflow.visible,
+                children: <Widget>[
+                  Card(
+                    elevation: 2.0,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Container(
+                      width: 300.0,
+                      height: 360.0,
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(
+                                top: 20.0,
+                                bottom: 20.0,
+                                left: 25.0,
+                                right: 25.0),
+                            child: TextFormField(
+                              focusNode: myFocusNodeName,
+                              controller: signupNameController,
+                              keyboardType: TextInputType.text,
+                              textCapitalization: TextCapitalization.words,
+                              style: TextStyle(
+                                  fontFamily: "WorkSansSemiBold",
+                                  fontSize: 16.0,
+                                  color: Colors.black),
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                icon: Icon(
+                                  FontAwesomeIcons.user,
+                                  color: Colors.black,
+                                ),
+                                hintText: "Name",
+                                hintStyle: TextStyle(
+                                    fontFamily: "WorkSansSemiBold",
+                                    fontSize: 16.0),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 250.0,
+                            height: 1.0,
+                            color: Colors.grey[400],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                                top: 20.0,
+                                bottom: 20.0,
+                                left: 25.0,
+                                right: 25.0),
+                            child: TextFormField(
+                              validator: (input) {
+                                if (input.isEmpty) {
+                                  return 'Provide an email';
+                                }
+                              },
+                              focusNode: myFocusNodeEmail,
+                              controller: signupEmailController,
+                              keyboardType: TextInputType.emailAddress,
+                              style: TextStyle(
+                                  fontFamily: "WorkSansSemiBold",
+                                  fontSize: 16.0,
+                                  color: Colors.black),
+                              onSaved: (input) => _emailSignUp = input,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                icon: Icon(
+                                  FontAwesomeIcons.envelope,
+                                  color: Colors.black,
+                                ),
+                                hintText: "Email Address",
+                                hintStyle: TextStyle(
+                                    fontFamily: "WorkSansSemiBold",
+                                    fontSize: 16.0),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 250.0,
+                            height: 1.0,
+                            color: Colors.grey[400],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                                top: 20.0,
+                                bottom: 20.0,
+                                left: 25.0,
+                                right: 25.0),
+                            child: TextFormField(
+                              validator: (input) {
+                                if (input.length < 6) {
+                                  return 'Longer password please';
+                                }
+                              },
+                              focusNode: myFocusNodePassword,
+                              controller: signupPasswordController,
+                              obscureText: _obscureTextSignup,
+                              style: TextStyle(
+                                  fontFamily: "WorkSansSemiBold",
+                                  fontSize: 16.0,
+                                  color: Colors.black),
+                              onSaved: (input) => _passwordSignUp = input,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                icon: Icon(
+                                  FontAwesomeIcons.lock,
+                                  color: Colors.black,
+                                ),
+                                hintText: "Password",
+                                hintStyle: TextStyle(
+                                    fontFamily: "WorkSansSemiBold",
+                                    fontSize: 16.0),
+                                suffixIcon: GestureDetector(
+                                  onTap: _toggleSignup,
+                                  child: Icon(
+                                    _obscureTextSignup
+                                        ? FontAwesomeIcons.eye
+                                        : FontAwesomeIcons.eyeSlash,
+                                    size: 15.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 250.0,
+                            height: 1.0,
+                            color: Colors.grey[400],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                                top: 20.0,
+                                bottom: 20.0,
+                                left: 25.0,
+                                right: 25.0),
+                            child: TextFormField(
+                              controller: signupConfirmPasswordController,
+                              obscureText: _obscureTextSignupConfirm,
+                              style: TextStyle(
+                                  fontFamily: "WorkSansSemiBold",
+                                  fontSize: 16.0,
+                                  color: Colors.black),
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                icon: Icon(
+                                  FontAwesomeIcons.lock,
+                                  color: Colors.black,
+                                ),
+                                hintText: "Confirmation",
+                                hintStyle: TextStyle(
+                                    fontFamily: "WorkSansSemiBold",
+                                    fontSize: 16.0),
+                                suffixIcon: GestureDetector(
+                                  onTap: _toggleSignupConfirm,
+                                  child: Icon(
+                                    _obscureTextSignupConfirm
+                                        ? FontAwesomeIcons.eye
+                                        : FontAwesomeIcons.eyeSlash,
+                                    size: 15.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 340.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          color: Theme.Colors.loginGradientStart,
+                          offset: Offset(1.0, 6.0),
+                          blurRadius: 20.0,
+                        ),
+                        BoxShadow(
+                          color: Theme.Colors.loginGradientEnd,
+                          offset: Offset(1.0, 6.0),
+                          blurRadius: 20.0,
+                        ),
+                      ],
+                      gradient: LinearGradient(
+                          colors: [
+                            Theme.Colors.loginGradientEnd,
+                            Theme.Colors.loginGradientStart
+                          ],
+                          begin: const FractionalOffset(0.2, 0.2),
+                          end: const FractionalOffset(1.0, 1.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    child: MaterialButton(
+                        highlightColor: Colors.transparent,
+                        splashColor: Theme.Colors.loginGradientEnd,
+                        //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 42.0),
+                          child: Text(
+                            "SIGN UP",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25.0,
+                                fontFamily: "WorkSansBold"),
+                          ),
+                        ),
+                        onPressed: () => _signUp(context)),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
